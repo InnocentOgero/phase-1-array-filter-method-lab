c59098dda2d5796ad98833372fb4fbb15b6a5245
InnocentOgero <innocent.ogero@student.moringaschool.com>
2023-09-30 17:55:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function  findMatching(driversArray,name){
+    const matched=driversArray.filter(driver=>driver.toLowerCase()===name.toLowerCase())
+    return matched
+
+}
+function fuzzyMatch(drivers,name){
+    const matched=drivers.filter(driver=>driver[0]===name[0])
+    return matched
+
+}
+function matchName(objectArray,name){
+    const matched=objectArray.filter(driver=>driver.name===name)
+    return matched
+
+}
